b99b67a526a423c9e645b036771fedaf346e7a0e
David Gilbert <dave@jfree.org>
2022-04-03 08:46:45 +0200

    fix: make better use of Radiation Animation API features - https://github.com/bric3/fireplace/pull/37#issuecomment-1083156071

--- fireplace-swing-animation/src/main/java/io/github/bric3/fireplace/flamegraph/ZoomAnimation.java
+++ fireplace-swing-animation/src/main/java/io/github/bric3/fireplace/flamegraph/ZoomAnimation.java
@@ -12,13 +12,17 @@ package io.github.bric3.fireplace.flamegraph;
 import io.github.bric3.fireplace.flamegraph.FlameGraph.FlameGraphCanvas;
 import io.github.bric3.fireplace.flamegraph.FlameGraph.ZoomAction;
 import org.pushingpixels.radiance.animation.api.Timeline;
-import org.pushingpixels.radiance.animation.api.callback.TimelineCallback;
 import org.pushingpixels.radiance.animation.api.ease.Sine;
+import org.pushingpixels.radiance.animation.api.swing.EventDispatchThreadTimelineCallbackAdapter;
 
 import javax.swing.*;
 import java.awt.*;
 
+/**
+ * A zoom action that incorporates animation using <a href="https://github.com/kirill-grouchnikov/radiance/blob/sunshine/docs/animation/animation.md">Radiance Animation</a>
+ */
 public class ZoomAnimation implements ZoomAction {
+
     private static final long ZOOM_ANIMATION_DURATION = 400L;
 
     /**
@@ -32,7 +36,6 @@ public class ZoomAnimation implements ZoomAction {
      * system property is set to disable it (`-Dfireplace.zoom.animation.disabled=true`).
      */
     private boolean animateZoomTransitions = !Boolean.getBoolean(ZOOM_ANIMATION_DISABLED_KEY);
-    ;
 
     public <T> void install(final FlameGraph<T> flameGraph) {
         flameGraph.overrideZoomAction(this);
@@ -75,26 +78,22 @@ public class ZoomAnimation implements ZoomAction {
         Timeline.builder()
                 .setDuration(ZOOM_ANIMATION_DURATION)
                 .setEase(new Sine())
-                .addCallback(new TimelineCallback() {
+                .addCallback(new EventDispatchThreadTimelineCallbackAdapter() {
                     @Override
                     public void onTimelineStateChanged(Timeline.TimelineState oldState, Timeline.TimelineState newState, float durationFraction, float timelinePosition) {
                         if (newState.equals(Timeline.TimelineState.DONE)) {
                             // throw in a final update to the target position, because the last pulse
                             // might not have reached exactly timelinePosition = 1.0...
-                            SwingUtilities.invokeLater(() -> {
-                                canvas.setSize(zoomTarget.bounds);
-                                viewPort.setViewPosition(zoomTarget.viewOffset);
-                            });
+                            canvas.setSize(zoomTarget.bounds);
+                            viewPort.setViewPosition(zoomTarget.viewOffset);
                         }
                     }
 
                     @Override
                     public void onTimelinePulse(float durationFraction, float timelinePosition) {
-                        SwingUtilities.invokeLater(() -> {
-                            canvas.setSize(new Dimension((int) (startW + timelinePosition * deltaW), (int) (startH + timelinePosition * deltaH)));
-                            Point pos = new Point(startX + (int) (timelinePosition * deltaX), startY + (int) (timelinePosition * deltaY));
-                            viewPort.setViewPosition(pos);
-                        });
+                        canvas.setSize(new Dimension((int) (startW + timelinePosition * deltaW), (int) (startH + timelinePosition * deltaH)));
+                        Point pos = new Point(startX + (int) (timelinePosition * deltaX), startY + (int) (timelinePosition * deltaY));
+                        viewPort.setViewPosition(pos);
                     }
                 }).build().playSkipping(3L);
         return true;
