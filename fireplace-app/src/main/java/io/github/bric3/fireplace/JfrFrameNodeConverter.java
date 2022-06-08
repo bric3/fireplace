bric3/fireplace

commit 55cd669decc802bfc659dd14529162d5cfd4c177

fix(fireplace): Incorrect child node width computation
Resolves: #61

fireplace-swing/src/main/java/io/github/bric3/fireplace/flamegraph/FrameBox.java
@@ -124,7 +124,7 @@ public class FrameBox<T> {
 
         depth++;
         var parentWidth = endX - startX;
-        var totalWeight = children.stream().mapToDouble(nodeWeight).sum();
+        var totalWeight = nodeWeight.applyAsDouble(fromNode);
         for (var node : children) {
             var nodeWidth = (nodeWeight.applyAsDouble(node) / totalWeight) * parentWidth;
             endX = startX + nodeWidth;

commit 61fcf319ea26d99341e674f4c752c66c94dee875

fix: Total node width was broken for JFR files after #61
the bug was introduced in 55cd669decc802bfc659dd14529162d5cfd4c177
"fix(fireplace): Incorrect child node width computation"
as part of #61,  ut it actually broke the JFR width
calculation. This commit fixes that.

fireplace-app/src/main/java/io/github/bric3/fireplace/JfrFrameNodeConverter.java
@@ -31,6 +31,7 @@ public class JfrFrameNodeConverter {
                 model.getRoot(),
                 Node::getChildren,
                 Node::getCumulativeWeight,
+                n -> n.getChildren().stream().mapToDouble(Node::getCumulativeWeight).sum(),
                 0.0d,
                 1.0d,
                 0

fireplace-swing/src/main/java/io/github/bric3/fireplace/flamegraph/FrameBox.java
@@ -115,6 +115,58 @@ public class FrameBox<T> {
             double startX,
             double endX,
             int depth) {
+        flattenAndCalculateCoordinate(
+                accumulator,
+                fromNode,
+                getChildren,
+                nodeWeight,
+                nodeWeight,
+                startX,
+                endX,
+                depth
+        );
+    }
+
+    /**
+     * Helper method that will flatten a tree by traversing it (DFS algorithm).
+     * <p>
+     * Example usage:
+     * <pre><code>
+     * var nodes = new ArrayList&lt;FrameBox&lt;Node&gt;&gt;();
+     *
+     * FrameBox.flattenAndCalculateCoordinate(
+     *         nodes,
+     *         model.getRoot(),
+     *         Node::getChildren,
+     *         Node::getCumulativeWeight,
+     *         0.0d,
+     *         1.0d,
+     *         0
+     * );
+     *
+     * flameGraph.setData(nodes, ...);
+     * </code></pre>
+     *
+     * @param <T>             The node type.
+     * @param accumulator     The flattened list of nodes.
+     * @param fromNode        Node to start from.
+     * @param getChildren     Get node children function.
+     * @param nodeWeight      The node weight function.
+     * @param totalNodeWeight
+     * @param startX          Start boundary on the X axis.
+     * @param endX            End boundary on the X axis.
+     * @param depth           The node depth.
+     */
+    public static <T> void flattenAndCalculateCoordinate(
+            List<FrameBox<T>> accumulator,
+            T fromNode,
+            Function<T, List<T>> getChildren,
+            ToDoubleFunction<T> nodeWeight,
+            ToDoubleFunction<T> totalNodeWeight,
+            double startX,
+            double endX,
+            int depth
+    ) {
         accumulator.add(new FrameBox<>(fromNode, startX, endX, depth));
 
         var children = getChildren.apply(fromNode);
@@ -124,7 +176,7 @@ public class FrameBox<T> {
 
         depth++;
         var parentWidth = endX - startX;
-        var totalWeight = nodeWeight.applyAsDouble(fromNode);
+        var totalWeight = totalNodeWeight.applyAsDouble(fromNode);
         for (var node : children) {
             var nodeWidth = (nodeWeight.applyAsDouble(node) / totalWeight) * parentWidth;
             endX = startX + nodeWidth;

fireplace-swt-experiment/src/main/java/io/github/bric3/fireplace/FirePlaceSwtMain.java
@@ -239,6 +239,7 @@ public class FirePlaceSwtMain {
                 model.getRoot(),
                 Node::getChildren,
                 Node::getCumulativeWeight,
+                n -> n.getChildren().stream().mapToDouble(Node::getCumulativeWeight).sum(),
                 0.0d,
                 1.0d,
                 0