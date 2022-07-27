60d7d1642a7a00c677590319c48d3379a622d963
Brice Dutheil <brice.dutheil@gmail.com>
2022-07-27 12:50:25 +0200

    chore(flamegraph): Removed commented code

--- fireplace-swing/src/main/java/io/github/bric3/fireplace/flamegraph/ZoomTarget.java
+++ fireplace-swing/src/main/java/io/github/bric3/fireplace/flamegraph/ZoomTarget.java
@@ -17,28 +17,6 @@ import java.util.Objects;
  * Represents a target for zooming.
  */
 public class ZoomTarget extends Rectangle {
-
-    // /** The bounds required to draw the whole content. */
-    // public Dimension dimension;
-
-    // /** The coordinates of the top-left corner of the frame that is the target of the zoom. */
-    // public Point viewOffset;
-    
-    // public Rectangle bounds;
-    //
-    // /**
-    //  * Creates a new zoom target.
-    //  *
-    //  * @param bounds The target canvas bounds.
-    //  * @param dimension  the bounds.
-    //  * @param viewOffset the view offset.
-    //  */
-    // public ZoomTarget(Rectangle bounds, Dimension dimension, Point viewOffset) {
-    //     this.bounds = Objects.requireNonNull(bounds);
-    //     this.dimension = Objects.requireNonNull(dimension);
-    //     this.viewOffset = Objects.requireNonNull(viewOffset);
-    // }
-
     public ZoomTarget(int x, int y, int width, int height) {
         super(x, y, width, height);
     }
