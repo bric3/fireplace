c5a0c384ba135a5afac02b0c96389747be2aedbb
Brice Dutheil <brice.dutheil@gmail.com>
2023-04-04 15:18:33 +0200

    fix: move JBLayeredPane.addLayer

--- fireplace-app/src/main/kotlin/io/github/bric3/fireplace/FirePlaceMain.kt
+++ fireplace-app/src/main/kotlin/io/github/bric3/fireplace/FirePlaceMain.kt
@@ -12,11 +12,11 @@ package io.github.bric3.fireplace
 import io.github.bric3.fireplace.ui.FrameResizeLabel
 import io.github.bric3.fireplace.ui.HudPanel
 import io.github.bric3.fireplace.ui.TitleBar
+import io.github.bric3.fireplace.ui.addLayer
 import io.github.bric3.fireplace.ui.debug.AssertiveRepaintManager
 import io.github.bric3.fireplace.ui.debug.CheckThreadViolationRepaintManager
 import io.github.bric3.fireplace.ui.debug.EventDispatchThreadHangMonitor
 import java.awt.BorderLayout
-import java.awt.Component
 import java.awt.Dimension
 import java.awt.event.WindowAdapter
 import java.awt.event.WindowEvent
@@ -114,30 +114,4 @@ private fun initUI(jfrBinder: JFRBinder, cliPaths: List<Path>) {
             isVisible = true
         }
     }
-}
-
-/**
- * This function is required in Kotlin for JLayeredPane to properly add components as layer.
- *
- * Otherwise, kotlin will treat [JLayeredPane.DEFAULT_LAYER] constants
- * as `int` and will instead call [`JLayeredPane.add(Component comp, int index)`][JLayeredPane.add]
- * instead of [`JLayeredPane.add(Component comp, Object constraints)`][JLayeredPane.add].
- *
- * Java will generate the following byt code
- *
- * ```
- * GETSTATIC javax/swing/JLayeredPane.MODAL_LAYER : Ljava/lang/Integer;
- * INVOKEVIRTUAL javax/swing/JLayeredPane.add (Ljava/awt/Component;Ljava/lang/Object;)V
- * ```
- *
- * While Kotlin will generate
- * ```
- * INVOKEVIRTUAL java/lang/Integer.intValue ()I
- * INVOKEVIRTUAL javax/swing/JLayeredPane.add (Ljava/awt/Component;I)Ljava/awt/Component;
- * ```
- *
- * This methods make sure the constraint is treated as an [Object].
- */
-fun JLayeredPane.addLayer(c: Component, constraint: Int) {
-    add(c, constraint as Any)
 }
--- fireplace-app/src/main/kotlin/io/github/bric3/fireplace/ui/SwingUtils.kt
+++ fireplace-app/src/main/kotlin/io/github/bric3/fireplace/ui/SwingUtils.kt
@@ -9,8 +9,10 @@
  */
 package io.github.bric3.fireplace.ui
 
+import java.awt.Component
 import java.awt.event.ComponentAdapter
 import java.awt.event.ComponentEvent
+import javax.swing.JLayeredPane
 import javax.swing.JSplitPane
 
 fun JSplitPane.autoSize(proportionalLocation: Double) {
@@ -23,4 +25,30 @@ fun JSplitPane.autoSize(proportionalLocation: Double) {
             }
         }
     })
+}
+
+/**
+ * This function is required in Kotlin for JLayeredPane to properly add components as layer.
+ *
+ * Otherwise, kotlin will treat [JLayeredPane.DEFAULT_LAYER] constants
+ * as `int` and will instead call [`JLayeredPane.add(Component comp, int index)`][JLayeredPane.add]
+ * instead of [`JLayeredPane.add(Component comp, Object constraints)`][JLayeredPane.add].
+ *
+ * Java will generate the following byt code
+ *
+ * ```
+ * GETSTATIC javax/swing/JLayeredPane.MODAL_LAYER : Ljava/lang/Integer;
+ * INVOKEVIRTUAL javax/swing/JLayeredPane.add (Ljava/awt/Component;Ljava/lang/Object;)V
+ * ```
+ *
+ * While Kotlin will generate
+ * ```
+ * INVOKEVIRTUAL java/lang/Integer.intValue ()I
+ * INVOKEVIRTUAL javax/swing/JLayeredPane.add (Ljava/awt/Component;I)Ljava/awt/Component;
+ * ```
+ *
+ * This methods make sure the constraint is treated as an [Object].
+ */
+fun JLayeredPane.addLayer(c: Component, constraint: Int) {
+    add(c, constraint as Any)
 }
